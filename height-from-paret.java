aab3ed89e01ea7caa71bf75d8bb3b746ebd45bb0
Anurag choudhary <anurag.jay1002@gmail.com>
2026-08-14 01:15:14 +0530

    Add height calculation for tree from parent array
    Implement a method to calculate the height of a tree based on its parent-child relationship represented in an array.

---
+++ height-from-paret.java
@@ -0,0 +1,38 @@
+/** You need to find the height of a tree given its parent-child relationship in an array
+where each index represents a node, and the value at that index represents its parent. 
+The root node has a value of -1.
+
+
+Input: [4, 3, 0, 6, 6, 3, -1, 0]
+Output: 4
+**/
+
+
+public class Main{
+
+  public static void main(int[] args) {
+      int[] arr = new int [] {4, 3, 0, 6, 6, 3, -1, 0};
+      List<List<Integer>> adj = new ArrayList<>();
+      for(int i=0; i<arr.length; i++) adj.add(new ArrayList<>());
+
+      int root = -1;
+      for(int i=0; i<arr.length; i++){
+        adj.get(arr[i]).add(i);
+        if(arr[i] == -1) root = i;
+      }
+      Queue<Integer> q = new ArrayList<>();
+      int height =0;
+      q.offer(root);
+      while(!q.isEmpty()){
+        height++;
+        int size = q.size();
+        for(int i=0; i<size; i++){
+            int node = q.poll();
+            for(int neigh : adj.get(node)) q.offer(neigh);
+        }
+      }
+
+      return height;
+      
+  }
+}
